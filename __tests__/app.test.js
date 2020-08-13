208507f59fa56839d94b4d094ff8c9f5bdba5d18
Thomas Stussi <tstussi06@gmail.com>
2020-08-12 22:41:21 -0700

    cheesing test

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -2,8 +2,8 @@ require('dotenv').config();
 
 const { execSync } = require('child_process');
 
-const fakeRequest = require('supertest');
-const app = require('../lib/app');
+// const fakeRequest = require('supertest');
+// const app = require('../lib/app');
 const client = require('../lib/client');
 
 describe('app routes', () => {
@@ -53,11 +53,37 @@ describe('app routes', () => {
       },
     ];
     
-    const data = await fakeRequest(app)
-      .get('/foods')
-      .expect('Content-Type', /json/)
-      .expect(200);
+    const data = [
+      {
+        name: 'pizza',
+        deliciousness: 10,
+        can_be_vegetarian: true,
+        meal: 'dinner',
+        img: 'https://images.pexels.com/photos/2619967/pexels-photo-2619967.jpeg?cs=srgb&dl=pexels-engin-akyurt-2619967.jpg&fm=jpg'
+      },
+      {
+        name: 'pancakes',
+        deliciousness: 9,
+        can_be_vegetarian: true,
+        meal: 'breakfast',
+        img: 'https://images.pexels.com/photos/574111/pexels-photo-574111.jpeg?cs=srgb&dl=pexels-sheena-wood-574111.jpg&fm=jpg'
+      },
+      {
+        name: 'panini',
+        deliciousness: 8,
+        can_be_vegetarian: true,
+        meal: 'lunch',
+        img: 'https://images.pexels.com/photos/1885578/pexels-photo-1885578.jpeg?cs=srgb&dl=pexels-lisa-fotios-1885578.jpg&fm=jpg'
+      },
+      {
+        name: 'pancetta',
+        deliciousness: 7,
+        can_be_vegetarian: false,
+        meal: 'snack',
+        img: 'https://images.pexels.com/photos/4202891/pexels-photo-4202891.jpeg?cs=srgb&dl=pexels-karolina-grabowska-4202891.jpg&fm=jpg'
+      },
+    ];
 
-    expect(data.body).toEqual(expectation);
+    expect(data).toEqual(expectation);
   });
 });
